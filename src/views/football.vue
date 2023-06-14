88844af1de0fcf73dcb73a8bbb9adf789c5e47d0
ZpowerQ <2364272910@qq.com>
2023-06-14 23:40:04 +0800

    add football

---
+++ src/views/football.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="football">
+    <div ref="footballRef"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import * as THREE from "three";
+import * as CANNON from "cannon-es";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+let footballRef = ref(null);
+
+onMouted(() => {});
+</script>
+
+<style scoped></style>
